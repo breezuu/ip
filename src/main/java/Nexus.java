6d0cccd49a5f98f1ba1b901aa193231df612a0b9
David <53869530+breezuu@users.noreply.github.com>
2026-01-22 17:51:23 +0800

    Add greeting

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,10 +1,26 @@
 public class Nexus {
+    private static final int WIDTH = 60;
+    private static final String BORDER = "─".repeat(WIDTH);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = """
+                  _   _                     \s
+                 | \\ | | _____  ___   _ ___\s
+                 |  \\| |/ _ \\ \\/ / | | / __|
+                 | |\\  |  __/>  <| |_| \\__ \\
+                 |_| \\_|\\___/_/\\_\\\\__,_|___/
+                     \s""";
+
+        printLine();
+        System.out.println(logo);
+        printLine();
+        System.out.println(" Hello! I'm Nexus.\n What can I do for you?");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println(BORDER);
     }
 }
